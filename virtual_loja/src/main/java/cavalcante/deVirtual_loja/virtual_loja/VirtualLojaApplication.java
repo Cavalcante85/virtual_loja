5115f0f754d5469475dc3667f2a82d4877464144
Cavalcante85 <scavalca85@gmai.com>
2024-09-26 18:14:46 -0300

    Subindo Projeto

---
+++ virtual_loja/src/main/java/cavalcante/deVirtual_loja/virtual_loja/VirtualLojaApplication.java
@@ -0,0 +1,13 @@
+package cavalcante.deVirtual_loja.virtual_loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualLojaApplication.class, args);
+	}
+
+}
---
+++ virtual_loja/src/test/java/cavalcante/deVirtual_loja/virtual_loja/VirtualLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package cavalcante.deVirtual_loja.virtual_loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
